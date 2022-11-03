e74b37d02436473d585f2b79ebedb0f42584b57c
satriasmita <satriasmita03@gmail.com>
2022-11-03 10:11:10 +0700

    Modifikasi menu agenda

--- backend/views/agenda/view.php
+++ backend/views/agenda/view.php
@@ -2,34 +2,30 @@
 
 use yii\helpers\Html;
 use yii\widgets\DetailView;
+use yii\helpers\StringHelper;
 
 /* @var $this yii\web\View */
 /* @var $model backend\models\Agenda */
 
-$this->title = $model->agenda_id;
+$this->title = 'Agenda : '.StringHelper::truncateWords(strip_tags($model->agenda_penjelasan),20);
 $this->params['breadcrumbs'][] = ['label' => 'Agendas', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
 ?>
 <div class="agenda-view">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        <?= Html::a('Update', ['update', 'id' => $model->agenda_id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->agenda_id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
-
+<div class="box box-solid box-warning">
+        <div class="box-header">
+            <h3 class="box-title"><?= Html::encode($this->title) ?></h3>
+            <div class="box-tools pull-right">
+                <?= Html::a('<i class="fa fa-fw fa-pencil-square"></i>Perbarui', ['update', 'id' => $model->agenda_id], ['class' => 'btn btn-success']) ?>
+                <?= Html::a('<i class="fa fa-fw fa-trash"></i>Hapus', ['delete', 'id' => $model->agenda_id], ['class' => 'btn btn-danger','data' => ['confirm' => 'Anda Yakin Ingin Menghapus Data Ini?', 'method' => 'post',],]) ?>
+            </div>
+        </div>
+        <div class="box-body">
     <?= DetailView::widget([
         'model' => $model,
         'attributes' => [
-            'agenda_id',
+            // 'agenda_id',
             'agenda_waktu',
             'agenda_tempat',
             'agenda_pukul',
